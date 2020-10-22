c10e69eadb845ef9ac61cea1ac6998782215bd5e
Giancarlo Zuliani <slaigox@gmail.com>
2020-10-22 17:24:08 +0200

    e-mail check exercise done

---
+++ email-check/js/script.js
@@ -0,0 +1,27 @@
+
+// array full of accepted mails
+var mailList = ["ciao@gmail.com" , "ciao@libero.it" , "ciao@hotmail.com" , "pippo@gmail.com" , "helloworld@gmail.com" , "pippo@hotmail.com" , "helloworld@hotmail.com" , "boolean@gmail.com" , "boolean@libero.it" , "boolean@hotmail.com" , "booleancareers@gmail.com" , "booleancareers@hotmail.com" , "1234@gmail.com" ];
+
+//switch variable
+
+
+
+
+function check(){
+  document.getElementById('result').innerHTML = "";
+  let x
+  let userMail = document.getElementById('mail').value;
+
+  for( i=0 ; i < mailList.length ; i++ ){
+
+    if(userMail === mailList[i]){
+       x = true;
+    }
+
+    if(x){
+      document.getElementById('result').innerHTML = "Password accepted";
+      }else {
+      document.getElementById('result').innerHTML = "Invalid password";
+    }
+  }
+}
